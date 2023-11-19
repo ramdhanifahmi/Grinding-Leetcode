ff43539c9b30481f4fd216e19f4f936bbde8a4a5
Fahmi Ramdhani <84432472+ramdhanifahmi@users.noreply.github.com>
2023-11-19 23:05:19 +0700

    Time: 1 ms (88.76%), Space: 39.7 MB (32.49%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num >= 10){
+            int result = 0;
+            while(num > 0){
+                result += num % 10;
+                num /= 10;
+            }
+            
+            num = result;
+    
+        }
+        
+    return num;
+    }
+}
