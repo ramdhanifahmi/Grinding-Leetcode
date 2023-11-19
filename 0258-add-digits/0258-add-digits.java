cfb259fff59330bef9cde0f6893be6d2868681f0
Fahmi Ramdhani <84432472+ramdhanifahmi@users.noreply.github.com>
2023-11-19 23:15:27 +0700

    Time: 0 ms (100.00%), Space: 40.7 MB (6.21%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,27 @@
 class Solution {
-    public int addDigits(int num) {
-        while(num >= 10){
-            int result = 0;
-            while(num > 0){
-                result += num % 10;
-                num /= 10;
-            }
+//     public int addDigits(int num) {
+//         while(num >= 10){
+//             int result = 0;
+//             while(num > 0){
+//                 result += num % 10;
+//                 num /= 10;
+//             }
             
-            num = result;
+//             num = result;
     
-        }
+//         }
         
-    return num;
+//     return num;
+//     }
+    
+    // cara 2 dengan digital root
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        } else if (num % 9 == 0) {
+            return 9;
+        } else {
+            return num % 9;
+        }
     }
 }
